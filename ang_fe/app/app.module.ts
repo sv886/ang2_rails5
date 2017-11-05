6fbed73f17c551a5fcf04a19793fdbec84223f01
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-11-05 11:40:24 -0500

    import new component, add declarations and bootstrap starting pt

--- ang_fe/app/app.module.ts
+++ ang_fe/app/app.module.ts
@@ -8,9 +8,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HomepageComponent } from './homepage.component';
+
 @NgModule({
   imports: [
     BrowserModule
+  ],
+  declarations: [
+    HomepageComponent
+  ],
+  bootstrap: [
+    HomepageComponent
   ]
 })
 

8a2af986838e62a4a452fd2f1c7234c21a6ddffd
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-11-25 14:10:25 -0500

    create proposal data model with optional attributes; update link text

---
+++ ang_fe/app/proposal/proposal.ts
@@ -0,0 +1,16 @@
+export class Proposal {
+  constructor(
+    public id?: number,
+    public customer?: string,
+    public portfolio_url: string = 'http://',
+    public tools?: string,
+    public estimated_hours?: number,
+    public hourly_rate?: number,
+    public weeks_to_complete?: number,
+    public client_email?: string,
+  ) {}
+}
+
+// Sample record might look asi:
+// 15, 'Abc Company', 'http://portfolio.joe-schmo.com', 'Ruby on Rails',
+// 150, 120, 15, 'john@doe.com'

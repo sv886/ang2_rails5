0c79821ed86b20a4af4c20523e55d45e7e020c18
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-11-26 11:23:24 -0500

    instantiate proposal objects in proposal-list component and render in view

--- ang_fe/app/proposal/proposal-list.component.ts
+++ ang_fe/app/proposal/proposal-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Proposal } from './proposal';
 
 @Component({
   moduleId: module.id,
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: 'proposal-list.component.html'
 })
 
-export class ProposalListComponent {}
+export class ProposalListComponent {
+  // Instantiate proposal objects w TypeScript
+  // Variable: Class    = create new obj w params defined in model
+  proposalOne: Proposal = new Proposal(15, 'Abc Company', 'http://portfolio.joe-schmo.com', 'Ruby on Rails',
+    150, 120, 15, 'john@doe.com')
+  proposalTwo: Proposal = new Proposal(3, '123 Company', 'http://portfolio.joe-schmo.com', 'Ruby on Rails',
+    150, 120, 15, 'john@doe.com')
+  proposalThree: Proposal = new Proposal(999, 'Algo Empresa', 'http://portfolio.joe-schmo.com', 'Ruby on Rails',
+    150, 120, 15, 'john@doe.com')
+}

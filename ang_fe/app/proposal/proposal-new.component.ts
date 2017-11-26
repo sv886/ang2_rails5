5f39e82f70b7d39da49e69610caaa492de27be62
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-11-26 12:34:56 -0500

    create new proposal ts and html files

---
+++ ang_fe/app/proposal/proposal-new.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-new',
+  templateUrl: 'proposal-new.component.html'
+})
+
+export class ProposalNewComponent {}

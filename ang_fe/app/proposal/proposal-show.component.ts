d5aad239d160603e75137dab189d9a2c41928e3d
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-12-01 20:42:36 -0500

    import libs for routing, get/set id params

--- ang_fe/app/proposal/proposal-show.component.ts
+++ ang_fe/app/proposal/proposal-show.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 import { Proposal } from './proposal';
 
 @Component({
@@ -7,4 +8,24 @@ import { Proposal } from './proposal';
   templateUrl: 'proposal-show.component.html'
 })
 
-export class ProposalShowComponent {}
+export class ProposalShowComponent implements OnInit {
+  id: number;
+  routeId: any;
+
+  constructor(
+    // using 'dependecy injection', let component know as soon as a class is
+    // instantiated (someone goes to show page), constructor is called and an
+    // instance of the activated route is stored in this arg for future use
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    // here we'll get id via params then set above
+    // subscribe gives us access to route params
+    this.routeId = this.route.params.subscribe(
+      params => {
+        this.id = +params['id'];
+      }
+    )
+  }
+}

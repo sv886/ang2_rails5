53c4e59649abfea1b649ade041a3355a62ed8b1d
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-12-01 20:24:10 -0500

    add proposal-show component ts and html files

---
+++ ang_fe/app/proposal/proposal-show.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { Proposal } from './proposal';
+
+@Component({
+  moduleId: module.id,
+  selector: 'proposal-show',
+  templateUrl: 'proposal-show.component.html'
+})
+
+export class ProposalShowComponent {}

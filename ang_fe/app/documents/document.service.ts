111b313006b4e595376f7222da09db6b9a5acba3
Sebastian Villarreal <Sebastian@sebastians-air.attlocal.net>
2017-12-15 19:44:35 -0500

    update import source for access to #map, add constructor note, add function to make doc api call, retrieve and format response

--- ang_fe/app/documents/document.service.ts
+++ ang_fe/app/documents/document.service.ts
@@ -8,14 +8,25 @@ import { Http, Response, Headers, RequestOptions } from '@angular/http';
 // The ReactiveX library for JavaScript, RxJS, is a library for reactive
 // programming using Observables to make it easier to compose asynchronous
 // or callback-based code.
-import { Observable } from 'rxjs/Observable';
+import { Observable } from 'rxjs/Rx';
 import { Document } from './document';
 
 @Injectable()
 export class DocumentService {
   private documentsUrl = 'http://localhost:3001/freelance_documents.json';
 
+  // constructor + dependency injection to make call to Http lib as soon as
+  // DocumentService class is instantiated
   constructor(
     private http: Http
   ) {}
+
+  // function will return an observable that returns an array of documents.
+  // Note the argument syntax: Observable<arg>
+  getDocuments(): Observable<Document[]> {
+    // connect to API using http lib from constructor above
+    return this.http.get(this.documentsUrl)
+                    // send request, get response and convert to json
+                    .map((response: Response) => <Document[]>response.json())
+  }
 }
